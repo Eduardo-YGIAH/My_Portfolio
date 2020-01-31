4364d504db8bce4111504179ee78f096d8699726
Eduardo Neto <edu.neto@icloud.com>
2020-01-31 23:45:30 +0000

    added files to github repository

---
+++ main-nav.js
@@ -0,0 +1,30 @@
+const navSlide = () => {
+  const burger = document.querySelector('.burger');
+  const nav = document.querySelector('.nav-links');
+  const navLinks = document.querySelectorAll('.nav-links li');
+  const wrapper = document.querySelector('.wrapper');
+
+  burger.addEventListener('click', () => {
+    wrapper.classList.toggle('wrapper-wide');
+    //Toggle Nav
+    nav.classList.toggle('nav-active');
+    //move burger
+
+    //Animate Links
+    navLinks.forEach((link, index) => {
+      if (link.style.animation) {
+        link.style.animation = '';
+      } else {
+        link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.5}s`;
+      }
+    });
+    //Burger Animation
+    burger.classList.toggle('toggle');
+  });
+};
+
+const app = () => {
+  navSlide();
+};
+
+app();
